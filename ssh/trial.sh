e652c8de98afcf9bcdd1f6285ad8af1881b12b88
BayuMOD <145831135+BayuMOD@users.noreply.github.com>
2023-10-29 09:44:35 +0700

    Update trial.sh

--- ssh/trial.sh
+++ ssh/trial.sh
@@ -101,5 +101,5 @@ echo -e "GET / HTTP/1.1[crlf]Host: ${domain}[crlf]Upgrade: websocket[crlf][crlf]
 echo -e "\033[0;34m◇━━━━━━━━━━━━━━━━━◇\033[0m"
 fi
 echo ""
-read -n 1 -s -r -p "PRESS [ ENTER ] KELUAR MENU"
+read -n 1 -s -r -p "SCRIPT BY BAYU & DANS"
 menu
