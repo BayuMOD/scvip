6ce79692941392d9721a57f03cc1c60da30c9bc5
BayuMOD <145831135+BayuMOD@users.noreply.github.com>
2023-11-04 13:19:46 +0700

    Update usernew.sh

--- ssh/usernew.sh
+++ ssh/usernew.sh
@@ -85,8 +85,8 @@ echo -e "Berakhir Pada: $expe" | tee -a /etc/log-create-user.log
 echo -e "\033[0;34m◇━━━━━━━━━━━━━━━━━◇\033[0m" | tee -a /etc/log-create-user.log
 echo -e "Payload Websocket NTLS :" | tee -a /etc/log-create-user.log
 echo -e "GET / HTTP/1.1[crlf]Host: [host][crlf]Connection: Upgrade[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]" | tee -a /etc/log-create-user.log
-echo -e "" | tee -a /etc/log-create-user.log
 echo -e "\033[0;34m◇━━━━━━━━━━━━━━━━━◇\033[0m"
+echo -e "" | tee -a /etc/log-create-user.log
 echo -e "Payload Websocket TLS  :" | tee -a /etc/log-create-user.log
 echo -e "GET https://[host]/ HTTP/1.1[crlf]Host: [host][crlf]Connection: Upgrade[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]" | tee -a /etc/log-create-user.log
 echo -e "\033[0;34m◇━━━━━━━━━━━━━━━━━◇\033[0m" | tee -a /etc/log-create-user.log
