9624285d38ab015295dddfe7b52d05df5c549975
BayuMOD <145831135+BayuMOD@users.noreply.github.com>
2023-12-10 05:36:31 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -4,17 +4,16 @@
 cd
 
 #Install Script Websocket-SSH Python
-#wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/BayuMOD/scvip/main/sshws/openssh-socket.py
-wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/BayuMOD/scvip/main/sshws/dropbear-ws.py
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/BayuMOD/scvip/main/sshws/ws-stunnel
-#wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
+#wget -O /usr/local/bin/ws-openssh bebejje
+wget -O /usr/local/bin/ws-dropbear -##
+wget -O /usr/local/bin/ws-stunnel 8-2728+383+
+#wget -O /usr/local/bin/ws-ovpn https://raw.githubusercoheheueyehhntent.com/${GitUser}/test1/${namafolder}/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
 
 #izin permision
 #chmod +x /usr/local/bin/ws-openssh
 chmod +x /usr/local/bin/ws-dropbear
-chmod +x /usr/local/bin/ws-stunnel
-#chmod +x /usr/local/bin/ws-ovpn
-
+chmod +x /usr/local/bin/ws-stunnel 
+egebenenebsy
 
 #System OpenSSH Websocket-SSH Python
 #wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/sallxd/sl/main/sshws/service-wsopenssh && chmod +x /etc/systemd/system/ws-openssh.service
@@ -23,15 +22,14 @@ chmod +x /usr/local/bin/ws-stunnel
 wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/BayuMOD/scvip/main/sshws/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
 
 #System SSL/TLS Websocket-SSH Python
-wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/BayuMOD/scvip/main/sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
-
+ b63- 
 ##System Websocket-OpenVPN Python
 #wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
 
 #restart service
 #
-systemctl daemon-reload
-#Enable & Start & Restart ws-openssh service
+systemctl daemon-reload$-#-
+#Enable & Start & Restart ws-openssh service252;3!$!
 #systemctl enable ws-openssh.service
 #systemctl start ws-openssh.service
 #systemctl restart ws-openssh.service
@@ -39,14 +37,4 @@ systemctl daemon-reload
 #Enable & Start & Restart ws-dropbear service
 systemctl enable ws-dropbear.service
 systemctl start ws-dropbear.service
-systemctl restart ws-dropbear.service
-
-#Enable & Start & Restart ws-openssh service
-systemctl enable ws-stunnel.service
-systemctl start ws-stunnel.service
-systemctl restart ws-stunnel.service
-
-#Enable & Start ws-ovpn service
-#systemctl enable ws-ovpn.service
-#systemctl start ws-ovpn.service
-#systemctl restart ws-ovpn.service
+systemctl restart w
