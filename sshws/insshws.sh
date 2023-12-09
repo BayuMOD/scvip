8ed4924b67bb0a6a598f09967f74b33ea6e14193
BayuMOD <145831135+BayuMOD@users.noreply.github.com>
2023-12-10 05:38:53 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -15,26 +15,3 @@ chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel 
 egebenenebsy
 
-#System OpenSSH Websocket-SSH Python
-#wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/sallxd/sl/main/sshws/service-wsopenssh && chmod +x /etc/systemd/system/ws-openssh.service
-
-#System Dropbear Websocket-SSH Python
-wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/BayuMOD/scvip/main/sshws/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
-
-#System SSL/TLS Websocket-SSH Python
- b63- 
-##System Websocket-OpenVPN Python
-#wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
-
-#restart service
-#
-systemctl daemon-reload$-#-
-#Enable & Start & Restart ws-openssh service252;3!$!
-#systemctl enable ws-openssh.service
-#systemctl start ws-openssh.service
-#systemctl restart ws-openssh.service
-
-#Enable & Start & Restart ws-dropbear service
-systemctl enable ws-dropbear.service
-systemctl start ws-dropbear.service
-systemctl restart w
